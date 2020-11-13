ee6c81969ed0f404d58f3f2e7ae56a4eb7d654f0
Martin Natano <natano@natano.net>
2020-11-13 20:14:32 +0100

    Add glad to the available video renderers for outfox
    StepMania 5.3 removed the windows specific d3d backend, but added a new
    cross-platform video renderer: glad. It is a more modern OpenGL
    renderer, but is not supported by old video cards.

--- Scripts/SL-OperatorMenuOptions.lua
+++ Scripts/SL-OperatorMenuOptions.lua
@@ -182,13 +182,16 @@ OperatorMenuOptionRows.VideoRenderer = function()
 	-- Windows also has d3d as a VideoRenderer, and the convention(?)
 	-- there is to list both in Preferences.ini, but only use the first
 	local architecture = HOOKS:GetArchName():lower()
-	if architecture:match("windows") then
+	if IsSMVersion(5, 3) then
+		table.insert(choices, "glad")
+		values = { "opengl,glad", "glad,opengl" }
+	elseif architecture:match("windows") then
 		table.insert(choices, "d3d")
 		values = { "opengl,d3d", "d3d,opengl" }
 	end
 
 	return {
-		Name = "VideoRenderer",
+		Name = IsSMVersion(5, 3) and "VideoRendererSM5.3" or "VideoRenderer",
 		Choices = choices,
 		LayoutType = "ShowAllInRow",
 		SelectType = "SelectOne",
